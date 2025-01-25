9a9fc0e1ed65edae5513589d3170abcf5e44a521
Louis Groux <louis.cal.groux@gmail.com>
2025-01-25 21:41:25 +0000

    Add barebone version

--- src/llm_cli/__main__.py
+++ src/llm_cli/__main__.py
@@ -1,9 +1,47 @@
+import argparse
 import sys
+import time
+from importlib.resources import files
+import os
 
-def main():
+from smolagents import HfApiModel
+
+def load_prompts():
+    res = {}
+    prompt_folder = files('llm_cli.prompts')
+    for prompt in prompt_folder.iterdir():
+        res[prompt.stem] = prompt.read_text()
+    return res
+
+PROMPTS = load_prompts()
+
+def run(args):
     """Console script for llm_cli."""
-    print("Edit src/llm_cli/__main__.py to extend CLI.")
+    model = HfApiModel(
+        token = os.getenv("HF_TOKEN")
+    )
+    msg = []
+    if args.query and args.prompt:
+        prompt = PROMPTS[args.prompt[0]]
+        msg.append({"role": "system", "content": prompt})
+
+    query = args.query or args.prompt
+    if args.input:
+        query += args.input.read()
+
+    msg.append({"role": "user", "content": query})
+    res = model(msg)
+    print(res.content)
     return 0
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", type=argparse.FileType("r"))
+    parser.add_argument("prompt", nargs=1, type=str)
+    parser.add_argument("query", nargs="?", type=str)
+    args = parser.parse_args()
+    run(args)
+
+
 if __name__ == "__main__":
-    sys.exit(main)
+    sys.exit(main())
