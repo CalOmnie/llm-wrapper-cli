63bc8e470bd3094bf937de8221d7a28c73c1de73
Louis Groux <louis.cal.groux@gmail.com>
2025-01-26 11:48:59 +0000

    Add proper argument handling of queries and input files

--- src/llm_cli/__main__.py
+++ src/llm_cli/__main__.py
@@ -21,13 +21,11 @@ def run(args):
         token = os.getenv("HF_TOKEN")
     )
     msg = []
-    query_lst = [*args.query, *args.extra]
-    if query_lst[0] in PROMPTS:
+    if args.query and args.query[0] in PROMPTS:
         prompt = PROMPTS[args.query[0]]
         msg.append({"role": "system", "content": prompt})
-        query_lst = query_lst[1:]
 
-    query = " ".join(query_lst)
+    query = " ".join(args.query)
     if args.input:
         for f in args.input:
             query += f"# {str(f.name)}\n"
@@ -41,8 +39,7 @@ def run(args):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input", type=argparse.FileType("r"), nargs="+")
-    parser.add_argument("-q", "--query", type=str, nargs="+")
-    parser.add_argument("extra", nargs="*", type=str)
+    parser.add_argument("query", nargs="*", type=str)
     args = parser.parse_args()
     print(args)
     run(args)
