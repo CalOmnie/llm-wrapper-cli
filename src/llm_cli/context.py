fa6e162c8fe3d876267f3c3b32c234a791e492c1
Louis Groux <louis.cal.groux@gmail.com>
2025-01-27 00:07:14 +0000

    Add context module

---
+++ src/llm_cli/context.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+import re
+
+from markitdown import MarkItDown
+
+URL_REGEX = re.compile(r'^https?://[^\s]+$')
+MARKDOWN_SUFFIX = {"pdf"}
+
+def generate_context(inputs: list[str]) -> str:
+    res = ""
+    for inp in inputs:
+        if URL_REGEX.match(inp):
+            res += extract_markdown(inp)
+        elif not (path := Path(inp)).exists():
+            raise ValueErrror(f"Invalid input {inp}")
+        else:
+            if should_markdown(path.suffix):
+                res += extract_markdown(path)
+            else:
+                res += "\n```\n" + read_file(path) + "\n```\n"
+    return res
+
+def extract_markdown(target: str) -> str:
+    md = MarkItDown()
+    result = md.convert(target)
+    return result.text_content
+
+def read_file(path: Path) -> str:
+    with path.open("rt") as f:
+        return f.read()
+
+def should_markdown(suffix: str) -> bool:
+    return suffix in MARKDOWN_SUFFIX
