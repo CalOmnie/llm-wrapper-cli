b945c23084e6602804168efe7f1640c5ea160718
Louis Groux <louis.cal.groux@gmail.com>
2025-02-15 12:21:03 +0000

    Add complete testing tool

--- src/llm_wrapper_cli/tools.py
+++ src/llm_wrapper_cli/tools.py
@@ -48,10 +48,11 @@ class FileWriteTool(Tool):
             f.write(content)
 
 
-class AddOneTest(Tool):
-    name = "add_one_test"
+class AddTest(Tool):
+    name = "add_test"
     description = """
-    This is a tool appending one test to a test file.
+    This is a tool appending a test to a test file, running sais test,
+    and removing it from the file if it fails.
     """
     inputs = {
         "path": {
@@ -65,16 +66,47 @@ class AddOneTest(Tool):
     }
     output_type = "null"
 
+    def __init__(self, run_cmd: str, *args, **kwargs):
+        self.run_cmd = run_cmd
+        super().__init__(*args, **kwargs)
+
     def forward(self, path: str, test_function: object):
+        fun_name = self.add_test(path, test_function)
+        output, returncode = self.run_test(path)
+        if returncode != 0:
+            self.delete_test(path, fun_name)
+        return output
+
+    def add_test(self, path: str, test_function: object) -> str:
         import ast
-        fun_code = ""
+        fun_code, fun_name = "", ""
         for param in test_function.__closure__:
             content = param.cell_contents
             if isinstance(content, ast.FunctionDef):
                 fun_code = ast.unparse(content)
+                fun_name = content.name
         with open(path, "at") as f:
             f.write("\n\n")
             f.write(fun_code)
+        return fun_name
+
+    def run_test(self, path: str):
+        import subprocess
+        full_cmd = f"{self.run_cmd} {path}".split()
+        res = subprocess.run(full_cmd, check=False, stdout=subprocess.PIPE)
+        output = res.stdout.decode()
+        returncode = res.returncode
+        return output, returncode
+
+    def delete_test(self, path: str, fun_name: str):
+        import ast
+        with open(path, "rt+") as f:
+            tree = ast.parse(f.read(), filename=path)
+            body = filter(lambda node: not (isinstance(node, ast.FunctionDef) and node.name == fun_name), tree.body)
+            tree.body = list(body)
+            f.seek(0)
+            f.write(ast.unparse(tree))
+            f.truncate()
 
 class RunTestFile(Tool):
     name = "run_test_file"
